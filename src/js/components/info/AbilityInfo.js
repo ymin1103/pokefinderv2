a993114161ba6d9bfbc52e1b68e89378317c72ba
ymin1103 <ymin1103@gmail.com>
2019-03-08 01:16:38 +0900

    fixed bug about pokedex entry_number and added link to repo

--- src/js/components/info/AbilityInfo.js
+++ src/js/components/info/AbilityInfo.js
@@ -6,10 +6,10 @@ const MakeAbilityData = (data) =>{
     const AbilityData = data.map((e)=>{
         return(
             <div className="d-flex flex-column flex-md-row justify-content-between border-bottom">
-                <p className={e.is_hidden===true ? "font-weight-bold" : ""}>
+                <p className={e.is_hidden === true ? "text-muted " : ""}>
                     {e.names[11-Language.korean].name}
                 </p>
-                <p className={e.is_hidden === true ? "font-weight-bold" : ""}>
+                <p className={e.is_hidden === true ? "text-muted" : ""}>
                     {e.flavor_text_entries[8].flavor_text}
                 </p>
             </div>
